b47e460115c14eb2f3887d6f3bec087a956eaa58
monktana <svengiebelmann@gmail.com>
2022-02-04 16:49:56 +0100

    test: coverage for name and id filtering on /types

--- tests/types.test.js
+++ tests/types.test.js
@@ -11,29 +11,245 @@ describe('/types', () => {
   it('recieves all pokemon types', async () => {
     const types = await httpClient.get(`types`).json();
 
-    expect(types.total).toBe(18);
     expect(types.results.length).toBe(18);
-    
     expect(types.results[0].name).not.toBeNull();
-    expect(types.results[0].url).not.toBeNull();
+  });
+
+  describe('name', () => {
+    it('can filter types by name', async () => {
+      const types = await httpClient.get(`types?name=fire`).json();
+      expect(types.results.length).toBe(1);
+
+      const fire = types.results[0];
+    
+      expect(fire.id).toBe(7);
+      expect(fire.name).toBe('fire');
+      expect(fire.damage_class).toBe('special');
+      expect(fire.matchups).not.toBeNull();
+      expect(fire.pokemon).not.toBeNull();
+      expect(fire.pokemon.find(pokemon => pokemon.name == 'charmander')).not.toBeNull();
+    });
+
+    it('recieves empty result for unknown name', async () => {
+      const types = await httpClient.get(`types?name=unknownName`).json();
+      expect(types.results.length).toBe(0);
+    });
+
+    it('recieves empty result for purely numerical value', async () => {
+      const types = await httpClient.get(`types?name=3`).json();
+      expect(types.results.length).toBe(0);
+    });
+  });
+
+  describe('ids', () => {
+    it('can filter pokemon by multiple ids', async () => {
+      const types = await httpClient.get(`types?ids=1,2,3`).json();
+      expect(types.results.length).toBe(3);
+
+      const bug = types.results[0];
+      const dark = types.results[1];
+      const dragon = types.results[2];
+    
+      expect(bug.id).toBe(1);
+      expect(bug.name).toBe('bug');
+      expect(bug.damage_class).toBe('physical');
+      expect(bug.matchups).not.toBeNull();
+      expect(bug.pokemon).not.toBeNull();
+      expect(bug.pokemon.find(pokemon => pokemon.name == 'caterpie')).not.toBeNull();
+    
+      expect(dark.id).toBe(2);
+      expect(dark.name).toBe('dark');
+      expect(dark.damage_class).toBe('special');
+      expect(dark.matchups).not.toBeNull();
+      expect(dark.pokemon).not.toBeNull();
+      expect(dark.pokemon.find(pokemon => pokemon.name == 'darkrai')).not.toBeNull();
+    
+      expect(dragon.id).toBe(3);
+      expect(dragon.name).toBe('dragon');
+      expect(dragon.damage_class).toBe('special');
+      expect(dragon.matchups).not.toBeNull();
+      expect(dragon.pokemon).not.toBeNull();
+      expect(dragon.pokemon.find(pokemon => pokemon.name == 'dragonite')).not.toBeNull();
+    });
+
+    it('can filter pokemon by a single id', async () => {
+      const types = await httpClient.get(`types?ids=1`).json();
+      expect(types.results.length).toBe(1);
+
+      const bug = types.results[0];
+    
+      expect(bug.id).toBe(1);
+      expect(bug.name).toBe('bug');
+      expect(bug.damage_class).toBe('physical');
+      expect(bug.matchups).not.toBeNull();
+      expect(bug.pokemon).not.toBeNull();
+      expect(bug.pokemon.find(pokemon => pokemon.name == 'caterpie')).not.toBeNull();
+    });
+
+    it('recieves an error if ids is used multiple times in querystring', async () => {
+      expect.assertions(1);
+
+      try {
+        await httpClient.get(`types?ids=1&ids=2`).json();
+      } catch (error) {
+        expect(error.response.statusCode).toBe(422);
+      }
+    });
+
+    it('recieves an error if ids are sent as an object', async () => {
+      expect.assertions(1);
+
+      try {
+        await httpClient.get(`types?ids[]=1`).json();
+      } catch (error) {
+        expect(error.response.statusCode).toBe(422);
+      }
+    });
+
+    it('recieves an error if ids is not in the expected format', async () => {
+      expect.assertions(3);
+
+      try {
+        await httpClient.get(`types?ids=1;2`).json();
+      } catch (error) {
+        expect(error.response.statusCode).toBe(422);
+      }
+
+      try {
+        await httpClient.get(`types?ids=1,2,a`).json();
+      } catch (error) {
+        expect(error.response.statusCode).toBe(422);
+      }
+
+      try {
+        await httpClient.get(`types?ids=[1,2]`).json();
+      } catch (error) {
+        expect(error.response.statusCode).toBe(422);
+      }
+    });
+  });
+
+  describe('limit', () => {
+
+    it('recieves the expected amout of elements based on limit parameter', async () => {
+      const types = await httpClient.get(`types?limit=10`).json();
+  
+      expect(types.results.length).toBe(10);
+    });
+
+    it(`recieves error for invalid limit parameter`, async () => {
+      expect.assertions(1);
+
+      try {
+        await httpClient.get(`types?limit=a`).json();
+      } catch (error) {
+        expect(error.response.statusCode).toBe(422);
+      }
+    });
+  
+    it(`recieves error for negative limit`, async () => {
+      expect.assertions(1);
+
+      try {
+        await httpClient.get(`types?limit=-1`).json();
+      } catch (error) {
+        expect(error.response.statusCode).toBe(422);
+      }
+    });
+  });
+
+  describe('start', () => {
+
+    it('recieves expected subset with start and limit', async () => {
+      const fullRange = await httpClient.get(`types?start=0&limit=10`).json();
+      const subSet = await httpClient.get(`types?start=6&limit=5`).json();
+
+      expect(fullRange.results).toEqual(expect.arrayContaining(subSet.results));
+    });
+
+    it('recieves nothing if start exceeds total', async () => {
+      const types = await httpClient.get(`types?start=1000`).json();
+  
+      expect(types.results.length).toBe(0);
+    });
+
+    it(`recieves error for invalid start parameter`, async () => {
+      expect.assertions(1);
+
+      try {
+        await httpClient.get(`types?start=a`).json();
+      } catch (error) {
+        expect(error.response.statusCode).toBe(422);
+      }
+    });
+  
+    it(`recieves error for negative start`, async () => {
+      expect.assertions(1);
+
+      try {
+        await httpClient.get(`types?start=-1`).json();
+      } catch (error) {
+        expect(error.response.statusCode).toBe(422);
+      }
+    });
   });
 
   describe("single", () => {
+    it('recieves bulbasaur for id 1', async () => {
+      const bulbasaur = await httpClient.get(`pokemon/1`).json();
+  
+      expect(bulbasaur.id).toBe(1);
+      expect(bulbasaur.name).toBe('bulbasaur');
+      expect(bulbasaur.types).not.toBeNull();
+      expect(bulbasaur.types.find(type => type.name == 'grass')).not.toBeNull();
+    });
+  
+    it('recieves 404 for a non numeric id', async () => {
+      try {
+        await httpClient.get(`pokemon/abc`).json();
+      } catch (error) {
+        expect(error.response.statusCode).toBe(404);
+      }
+    });
+  
+    it('recieves 404 for a non integer id', async () => {
+      try {
+        await httpClient.get(`pokemon/1.5`).json();
+      } catch (error) {
+        expect(error.response.statusCode).toBe(404);
+      }
+    });
+  
+    it('recieves 404 for a negative id', async () => {
+      try {
+        await httpClient.get(`pokemon/-1`).json();
+      } catch (error) {
+        expect(error.response.statusCode).toBe(404);
+      }
+    });
+  
+    it('recieves 404 for an id which exceeds the total amount', async () => {
+      try {
+        await httpClient.get(`pokemon/1000`).json();
+      } catch (error) {
+        expect(error.response.statusCode).toBe(404);
+      }
+    });
+  })
+
+  describe("single", () => {
     it('recieves bug for /1', async () => {
-      const bugType = await httpClient.get(`types/1`).json();
-  
-      expect(bugType.id).toBe(1);
-      expect(bugType.name).toBe('bug');
+      const bug = await httpClient.get(`types/1`).json();
+
+      expect(bug.id).toBe(1);
+      expect(bug.name).toBe('bug');
+      expect(bug.damage_class).toBe('physical');
+      expect(bug.matchups).not.toBeNull();
+      expect(bug.pokemon).not.toBeNull();
+      expect(bug.pokemon.find(pokemon => pokemon.name == 'caterpie')).not.toBeNull();
     });
   
-    it('recieves bug for /bug', async () => {
-      const bugType = await httpClient.get(`types/bug`).json();
-  
-      expect(bugType.id).toBe(1);
-      expect(bugType.name).toBe('bug');
-    });
-  
-    it('recieves 404 for /abc', async () => {
+    it('recieves 404 for a non numeric id', async () => {
       try {
         await httpClient.get(`types/abc`).json();
       } catch (error) {
@@ -41,9 +257,25 @@ describe('/types', () => {
       }
     });
   
-    it('recieves 404 for /1000', async () => {
+    it('recieves 404 for a non integer id', async () => {
       try {
-        await httpClient.get(`types/100`).json();
+        await httpClient.get(`types/1.5`).json();
+      } catch (error) {
+        expect(error.response.statusCode).toBe(404);
+      }
+    });
+  
+    it('recieves 404 for a negative id', async () => {
+      try {
+        await httpClient.get(`types/-1`).json();
+      } catch (error) {
+        expect(error.response.statusCode).toBe(404);
+      }
+    });
+  
+    it('recieves 404 for an id which exceeds the total amount', async () => {
+      try {
+        await httpClient.get(`types/1000`).json();
       } catch (error) {
         expect(error.response.statusCode).toBe(404);
       }
